01ec88c4c221a8d5324b0e5dbcb880197aad1b9a
Alexander Chaika <manti.by@gmail.com>
2023-10-03 23:58:20 +0300

    Add pagination to compare tool

--- mgallery/compare.py
+++ mgallery/compare.py
@@ -32,8 +32,6 @@ class DuplicatesBox(Gtk.Box):
         grid = Gtk.Grid()
         grid.set_column_spacing(5)
         grid.set_row_spacing(0)
-        grid.set_row_homogeneous(True)
-        grid.set_column_homogeneous(True)
         for index, image in enumerate(images):
             if ".gif" in image["name"]:
                 continue
@@ -97,44 +95,64 @@ class DuplicatesGrid(Gtk.Grid):
     def __init__(self, duplicates: dict):
         super().__init__()
 
-        self.set_column_spacing(5)
-        self.set_row_spacing(5)
-        self.set_hexpand(True)
+        self.set_column_spacing(20)
+        self.set_row_spacing(50)
 
         left = top = 0
         for phash, images in duplicates.items():
             duplicates_box = DuplicatesBox(images)
             self.attach(duplicates_box, left, top, 1, 1)
             left += 1
-            if (left + 1) % 3 == 0:
+            if (left + 1) % 5 == 0:
                 left = 0
                 top += 1
             self.duplicates_boxes.append(duplicates_box)
 
 
 class DuplicatesApp(Gtk.VBox):
-    def __init__(self, duplicates: dict):
-        logger.info(f"Compare {len(duplicates)} images")
-
+    def __init__(self, database: Database, duplicates: dict):
         super().__init__()
 
-        scrolled_window = Gtk.ScrolledWindow()
-        self.add(scrolled_window)
+        self.database = database
+        self.duplicates = duplicates
 
-        self.duplicates_grid = DuplicatesGrid(duplicates)
-        scrolled_window.add(self.duplicates_grid)
+        self.page_size = 12
+        self.current_page = 1
+        self.total_pages = len(duplicates) // self.page_size + 1
 
-        buttons_grid = Gtk.Grid()
-        buttons_grid.set_column_spacing(10)
-        buttons_grid.set_row_spacing(10)
+        self.stack = Gtk.Stack()
+        self.stack.set_border_width(10)
+        for page in range(1, self.total_pages + 1):
+            start = (page - 1) * self.page_size
+            end = start + self.page_size
+            duplicates_slice = dict(list(self.duplicates.items())[start:end])
+            duplicates_grid = DuplicatesGrid(duplicates_slice)
+            self.stack.add_titled(duplicates_grid, f"page-{page}", f"{page}")
+        self.add(self.stack)
 
-        delete_button = Gtk.Button(label="Delete")
-        delete_button.connect("clicked", self.delete_images)
-        buttons_grid.attach(delete_button, 0, 0, 10, 1)
+        self.buttons_grid = Gtk.Grid(column_spacing=10, row_spacing=10)
 
-        self.pack_start(buttons_grid, False, False, 10)
+        self.delete_button = Gtk.Button(label="Delete")
+        self.delete_button.connect("clicked", self.delete_images)
+        self.buttons_grid.attach(self.delete_button, 0, 0, 10, 1)
 
-        self.database = Database()
+        self.spacer_label = Gtk.Label()
+        self.spacer_label.set_hexpand(True)
+        self.buttons_grid.attach(self.spacer_label, 20, 0, 20, 1)
+
+        self.prev_page_button = Gtk.Button(label="<< Prev")
+        self.prev_page_button.connect("clicked", self.prev_page)
+        self.buttons_grid.attach(self.prev_page_button, 50, 0, 10, 1)
+
+        self.pagination_label = Gtk.Label()
+        self.pagination_label.set_text(f"{self.current_page} / {self.total_pages}")
+        self.pagination_label.set_justify(Gtk.Justification.CENTER)
+        self.buttons_grid.attach(self.pagination_label, 60, 0, 5, 1)
+
+        self.next_page_button = Gtk.Button(label="Next >>")
+        self.next_page_button.connect("clicked", self.next_page)
+        self.buttons_grid.attach(self.next_page_button, 75, 0, 10, 1)
+        self.pack_start(self.buttons_grid, False, False, 0)
 
     def delete_images(self, button: Gtk.Button):
         for path, name in self.duplicates_grid.duplicates_boxes[0].files_to_delete:
@@ -146,17 +164,27 @@ class DuplicatesApp(Gtk.VBox):
                 continue
             logger.error(f"Can't find a file {file_name}")
 
+    def next_page(self, *args):
+        self.current_page = self.total_pages if self.current_page == self.total_pages else self.current_page + 1
+        self.pagination_label.set_text(f"{self.current_page} / {self.total_pages}")
+        self.stack.set_visible_child_name(f"page-{self.current_page}")
 
-def build_image_compare_window(window: Gtk.Window):
-    duplicates = Database().duplicates()
-    duplicates_app = DuplicatesApp(duplicates)
-    window.add(duplicates_app)
+    def prev_page(self, *args):
+        self.current_page = 1 if self.current_page == 1 else self.current_page - 1
+        self.pagination_label.set_text(f"{self.current_page} / {self.total_pages}")
+        self.stack.set_visible_child_name(f"page-{self.current_page}")
 
 
-def run_compare(width: int = 1600, height: int = 800):
-    window = DuplicatesWindow()
-    window.set_default_size(width, height)
-    build_image_compare_window(window)
+def run_compare(width: int = 1200, height: int = 800):
+    database = Database()
+    duplicates = database.duplicates()
+    logger.info(f"Compare {len(duplicates)} images")
+
+    window = Gtk.Window(title="Duplicated Images", default_width=width, default_height=height)
+    window.set_border_width(20)
+
+    app = DuplicatesApp(database, duplicates)
+    window.add(app)
 
     window.connect("destroy", Gtk.main_quit)
     window.show_all()
